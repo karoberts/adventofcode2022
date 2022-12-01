75a7aae4be80c7a48562112154654b9a9448f76d
kroberts <cppwriter@yahoo.com>
2022-12-01 12:15:57 -0800

    cleanup

--- 01.py
+++ 01.py
@@ -1,15 +1,12 @@
 with open("01.txt") as f:
     elves = [0]
-    cur_elf = 0
     for line in (l.strip() for l in f):
         if len(line) == 0:
-            cur_elf += 1
             elves.append(0)
         else:
-            elves[cur_elf] += int(line)
+            elves[-1] += int(line)
     
-m = max(enumerate(elves), key = lambda x:x[1])
-print("part1", m[1])
+print("part1", max(elves))
 
-top_three = list(sorted(elves, key=lambda x:-x))[0:3]
+top_three = (sorted(elves, key=lambda x:-x))[0:3]
 print("part2", sum(top_three))
