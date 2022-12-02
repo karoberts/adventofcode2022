ff6ea5d99560db374d7efb06aa8c5d5633a941ad
kroberts <cppwriter@yahoo.com>
2022-12-02 08:39:55 -0800

    2-1

---
+++ 02.py
@@ -0,0 +1,29 @@
+games = []
+
+# 1 = rock
+# 2 = paper
+# 3 = scissors
+
+aMap = { 'A': 1, 'B': 2, 'C': 3 }
+bMap = { 'X': 1, 'Y': 2, 'Z': 3 }
+
+winMap = { 
+    (1, 1): 3, 
+    (1, 2): 6,  
+    (1, 3): 0,  
+
+    (2, 1): 0, 
+    (2, 2): 3,  
+    (2, 3): 6,  
+
+    (3, 1): 6, 
+    (3, 2): 0,  
+    (3, 3): 3,  
+}
+
+with open('02.txt') as f:
+    for line in f.read().splitlines():
+        games.append((aMap[line[0]], bMap[line[2]]))
+
+score = sum(map(lambda x: x[1] + winMap[x], games))
+print('part1', score)
