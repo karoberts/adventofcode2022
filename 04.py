ac8483b2d297e85808664e0c33f5437492437c21
kroberts <cppwriter@yahoo.com>
2022-12-04 12:16:48 -0800

    4-2

--- 04.py
+++ 04.py
@@ -1,4 +1,6 @@
 import re
+from collections import defaultdict
+from typing import List, Set
 
 groups = []
 
@@ -17,3 +19,16 @@ def is_inside(g1, g2):
 
 p1 = sum((1 for g in groups if is_inside(g[0], g[1])))
 print('part1', p1)
+
+covereds = []
+for g in groups:
+    c = set()
+    for i in range(g[0][0], g[0][1] + 1): 
+        c.add(i)
+    for i in range(g[1][0], g[1][1] + 1): 
+        if i in c:
+            covereds.append(True)
+            break
+
+p2 = sum((1 for x in covereds if x))
+print('part2', p2)
