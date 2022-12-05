e3d7b426e3d6f9dba8dd3c6a0b27bc22aff6a08c
kroberts <cppwriter@yahoo.com>
2022-12-05 09:22:54 -0800

    5-2

--- 05.py
+++ 05.py
@@ -22,6 +22,10 @@ with open('05.txt') as f:
             m = re.match(r'^move (\d+) from (\d) to (\d)$', line)
             commands.append((int(m.group(1)), int(m.group(2)), int(m.group(3))))
 
+stacks_2 = {}
+for s in stacks.items():
+    stacks_2[s[0]] = s[1].copy()
+
 def print_stacks(s):
     for i in range(1, len(s) + 1):
         print('stack', i, s[i])
@@ -44,3 +48,21 @@ p1 = ''
 for i in range(1, len(stacks) + 1):
     p1 += stacks[i][-1]
 print('part1', p1)
+
+for cmd in commands:
+    amt = cmd[0]
+    mv_from = cmd[1]
+    mv_to = cmd[2]
+
+    items = []
+    for i in range(0, amt):
+        items.append(stacks_2[mv_from].pop())
+
+    for i in reversed(items):
+        stacks_2[mv_to].append(i)
+
+
+p2 = ''
+for i in range(1, len(stacks_2) + 1):
+    p2 += stacks_2[i][-1]
+print('part2', p2)
