5f67779c330b4db5a30f6f1a4d0ff43b341b52c3
kroberts <cppwriter@yahoo.com>
2022-12-06 09:05:48 -0800

    cleanup to not maintain the window

--- 06.py
+++ 06.py
@@ -4,14 +4,9 @@ with open('06.txt') as f:
     buffer = f.readline()
 
 def find_marker(buffer, ucount):
-    window_list = []
-    for i, c in enumerate(buffer):
-        if len(window_list) == ucount:
-            window_list.pop(0)
-        window_list.append(c)
-
-        if len(window_list) == ucount and len(set(window_list)) == ucount:
-            return i + 1
+    for i in range(0, len(buffer)):
+        if i >= ucount and len(set(buffer[i-ucount:i])) == ucount:
+            return i
     return -1
 
 print('part1', find_marker(buffer, 4))
