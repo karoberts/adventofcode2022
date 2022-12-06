0a72502e11ef88ca405962c5a9a8c99246ae4ed5
kroberts <cppwriter@yahoo.com>
2022-12-06 08:00:38 -0800

    6-2

--- 06.py
+++ 06.py
@@ -3,16 +3,16 @@ buffer = 'nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg'
 with open('06.txt') as f:
     buffer = f.readline()
 
-window_set = set()
-window_list = []
-p1 = -1
-for i, c in enumerate(buffer):
-    if len(window_list) == 4:
-        window_list.pop(0)
-    window_list.append(c)
+def find_marker(buffer, ucount):
+    window_list = []
+    for i, c in enumerate(buffer):
+        if len(window_list) == ucount:
+            window_list.pop(0)
+        window_list.append(c)
 
-    if len(window_list) == 4 and len(set(window_list)) == 4:
-        p1 = i + 1
-        break
+        if len(window_list) == ucount and len(set(window_list)) == ucount:
+            return i + 1
+    return -1
 
-print('part1', p1)
+print('part1', find_marker(buffer, 4))
+print('part2', find_marker(buffer, 14))
