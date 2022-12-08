028b4d6143d33d7e87ea0494d39f42443138559d
kroberts <cppwriter@yahoo.com>
2022-12-08 09:44:55 -0800

    8-2

--- 08.py
+++ 08.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
 from math import prod
-from typing import DefaultDict, Set
+from typing import DefaultDict, List, Set
 
 grid = defaultdict(lambda:0)
 
@@ -44,13 +44,31 @@ for x in range(0, max_x + 1):
 
 print('part1', len(visible))
 
-"""
+def count_look(g:DefaultDict, house:tuple, delta:tuple, height:int) -> int:
+    def apply(p:tuple, d:tuple):
+        return (p[0] + d[0], p[1] + d[1])
+
+    pos = apply(house, delta)
+    cur_h = height
+    vis = []
+    while pos[0] >= 0 and pos[1] >= 0 and pos[0] <= max_x and pos[1] <= max_y:
+        vis.append(pos)
+        if g[pos] >= cur_h:
+            return vis
+        pos = apply(pos, delta)
+    return vis
+
+scores = []
+
 for y in range(0, max_y + 1):
     for x in range(0, max_x + 1):
-        c = (x,y)
-        if c in visible:
-            print('*', end='')
-        else:
-            print(grid[(x,y)], end='')
-    print()
-"""
+        house = (x,y)
+        v1 = count_look(grid, house, (-1, 0), grid[house]) 
+        v2 = count_look(grid, house, (0, -1), grid[house]) 
+        v3 = count_look(grid, house, (1, 0), grid[house])
+        v4 = count_look(grid, house, (0, 1), grid[house])
+        score = len(v1) * len(v2) * len(v3) * len(v4)
+        scores.append(score)
+
+print('part2', max(scores))
+
