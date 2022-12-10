4424f8915c62c58bdcffbf8ccd7472e9d6acd450
kroberts <cppwriter@yahoo.com>
2022-12-09 16:30:21 -0800

    big cleanup and solves both parts

--- 09-2.py
+++ 09-2.py
@@ -3,11 +3,6 @@ import math
 with open('09.txt') as f:
     moves = [(y[0], int(y[1])) for y in (x.split(' ') for x in f.read().splitlines())]
 
-head = (0, 0)
-rope = [head for _ in range(1, 10)]
-tail_visited = set()
-tail_visited.add(rope[-1])
-
 """
 -- --
 -...-
@@ -35,16 +30,18 @@ pos_map = {
     (1, -2): (-1, 1),
 }
 
+delta_map = {
+    'R': (1, 0),
+    'L': (-1, 0),
+    'U': (0, 1),
+    'D': (0, -1)
+}
+
 def is_adj(p1, p2):
-    if p1 == p2:
-        return True
-    if (p1[1] == p2[1]) and (p1[0] == p2[0] - 1 or p1[0] == p2[0] + 1):
-        return True
-    if (p1[0] == p2[0]) and (p1[1] == p2[1] - 1 or p1[1] == p2[1] + 1):
-        return True
-    if (abs(p1[0] - p2[0]) == 1 and abs(p1[1] - p2[1]) == 1):
-        return True
-    return False
+    x_d = abs(p1[0] - p2[0])
+    y_d = abs(p1[1] - p2[1])
+    sd = x_d + y_d
+    return sd == 1 or sd == 0
 
 def apply(p:tuple, d:tuple):
     return (p[0] + d[0], p[1] + d[1])
@@ -71,52 +68,41 @@ def print_grid(h, r, v):
         print()
     print()
 
-for move in moves:
-    match move[0]:
-        case 'R':
-            d = (1, 0)
-        case 'L':
-            d = (-1, 0)
-        case 'U':
-            d = (0, 1)
-        case 'D':
-            d = (0, -1)
+def run(rope_len:int):
+    head = (0, 0)
+    rope = [head for _ in range(1, rope_len + 1)]
+    tail_visited = set()
+    tail_visited.add(rope[-1])
 
-    #print(move)
+    for move in moves:
+        d = delta_map[move[0]]
 
-    for i in range(0, move[1]):
-        head = apply(head, d)
+        for i in range(0, move[1]):
+            head = apply(head, d)
 
-        #print_grid(head, rope, tail_visited)
+            prev = head
+            for i in range(0, len(rope)):
+                knot = rope[i]
+                if is_adj(prev, knot):
+                    prev = rope[i]
+                    continue
 
-        prev = head
-        for i in range(0, len(rope)):
-            knot = rope[i]
-            if is_adj(prev, knot):
+                if knot[0] == prev[0]:
+                    kd = math.copysign(1, prev[1] - knot[1])
+                    knot = apply(knot, (0, kd))
+                elif knot[1] == prev[1]:
+                    kd = math.copysign(1, prev[0] - knot[0])
+                    knot = apply(knot, (kd, 0))
+                else:
+                    for pm in pos_map.items():
+                        if knot == apply(prev, pm[0]):
+                            knot = apply(knot, pm[1])
+                            break
+                rope[i] = knot
                 prev = rope[i]
-                continue
 
-            if knot[0] == prev[0]:
-                if knot[1] < prev[1]:
-                    knot = apply(knot, (0, 1))
-                else:
-                    knot = apply(knot, (0, -1))
-            elif knot[1] == prev[1]:
-                if knot[0] < prev[0]:
-                    knot = apply(knot, (1, 0))
-                else:
-                    knot = apply(knot, (-1, 0))
-            else:
-                for pm in pos_map.items():
-                    test = apply(prev, pm[0])
-                    if knot == test:
-                        knot = apply(knot, pm[1])
-            rope[i] = knot
-            prev = rope[i]
+            tail_visited.add(rope[-1])
+    return len(tail_visited)
 
-            #print_grid(head, rope, tail_visited)
-
-        tail_visited.add(rope[-1])
-
-#print(tail_visited)
-print('part2', len(tail_visited))
+print('part1', run(1))
+print('part2', run(9))
