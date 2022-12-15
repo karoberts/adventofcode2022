58aa595ebf43286933181dd15337a44dde2f6108
kroberts <cppwriter@yahoo.com>
2022-12-15 11:07:56 -0800

    cut the time for 15-1 from 52s to 8s

--- 15.py
+++ 15.py
@@ -1,5 +1,5 @@
-from collections import defaultdict
 import re
+from line_profiler import LineProfiler
 
 X = 0
 Y = 1
@@ -9,6 +9,7 @@ def manhat_dist(c1:tuple, c2:tuple) -> int:
 
 sensors = []
 beacons = []
+dists_map = {}
 dists = []
 min_y = 0
 min_x = 0
@@ -21,11 +22,15 @@ with open('15.txt') as f:
         sensors.append( (int(m.group(1)), int(m.group(2))) )
         beacons.append( (int(m.group(3)), int(m.group(4))) )
         dists.append(manhat_dist(sensors[-1], beacons[-1]))
+        dists_map[sensors[-1]] = dists[-1]
 
-        min_y = min(min_y, sensors[-1][Y] - dists[-1], beacons[-1][Y] - dists[-1])
-        min_x = min(min_x, sensors[-1][X] - dists[-1], beacons[-1][X] - dists[-1])
-        max_y = max(max_y, sensors[-1][Y] + dists[-1], beacons[-1][Y] + dists[-1])
-        max_x = max(max_x, sensors[-1][X] + dists[-1], beacons[-1][X] + dists[-1])
+        xdist = abs(sensors[-1][X] - beacons[-1][X])
+        ydist = abs(sensors[-1][Y] - beacons[-1][Y])
+
+        min_y = min(min_y, sensors[-1][Y] - ydist, beacons[-1][Y] - ydist)
+        min_x = min(min_x, sensors[-1][X] - xdist, beacons[-1][X] - xdist)
+        max_y = max(max_y, sensors[-1][Y] + ydist, beacons[-1][Y] + ydist)
+        max_x = max(max_x, sensors[-1][X] + xdist, beacons[-1][X] + xdist)
 
 if max_x - min_x < 1_000_000:
     y_target = 10
@@ -34,6 +39,10 @@ else:
 
 beacon_set = set(beacons)
 sensor_set = set(sensors)
+
+sensor_sorted = sorted(sensors, key=lambda s:abs(s[Y] - y_target))
+dists_sorted = [dists_map[s] for s in sensor_sorted]
+ydists_sorted = [abs(s[Y] - y_target) for s in sensor_sorted]
 
 """
 for y in range(min_y, max_y + 1):
@@ -47,29 +56,24 @@ for y in range(min_y, max_y + 1):
     print()
 """
 
-open_spots = 0
+def p1():
+    open_spots = 0
+    for x in range(min_x, max_x + 1):
+        c = (x, y_target)
+        if c not in sensor_set and c not in beacon_set:
+            for i in range(0, len(sensors)):
+                xd = abs(c[X] - sensor_sorted[i][X]) + ydists_sorted[i]
+                if xd <= dists_sorted[i]:
+                    open_spots += 1
+                    break
 
-for x in range(min_x, max_x + 1):
-    ok = 0
-    c = (x, y_target)
-    if c in sensor_set or c in beacon_set:
-        ok = len(sensors)
-    else:
-        for i in range(0, len(sensors)):
-            d = dists[i]
-            s = sensors[i]
-            b = beacons[i]
+    print('part1', open_spots)
 
-            xd = manhat_dist(c, s)
-            if xd <= d:
-                break
-            ok += 1
-        
-    if ok != len(sensors):
-        #print((x, y_target))
-        open_spots += 1
+p1()
 
-print('part1', open_spots)
-            
+#lp = LineProfiler()
+#lp_w = lp(p1)
+#lp_w()
+#lp.print_stats()            
 
 
